898af50ba5f3d883c00c8eba3d5dd92f4b0d7dd1
thawzinoomyanmar <thaw.zin.oo12@gmail.com>
2018-06-17 13:21:50 +0630

    prjcollectionview

---
+++ prj/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  prj
+//
+//  Created by user on 6/17/18.
+//  Copyright © 2018 App Dev. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var Image: UIImageView!
+    
+}
--- prj/ViewController.swift
+++ prj/ViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+    let Image = [UIImage(named: "t"),UIImage(named: "t"),UIImage(named: "t")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Image.count
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+   cell.Image.image = Image[indexPath.row]
+        return cell
+    }
+
 
 
 }
