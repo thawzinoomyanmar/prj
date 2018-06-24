0616eca3bf86dbac504305efb96fb19010461440
thawzinoomyanmar <thaw.zin.oo12@gmail.com>
2018-06-24 20:59:51 +0630

    gardientviewandfacebookauth

--- prj/ViewController.swift
+++ prj/ViewController.swift
@@ -11,10 +11,48 @@ import Firebase
 import FBSDKLoginKit
 import FBSDKCoreKit
 import FirebaseStorage
+import SwiftyJSON
 class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
     let Image = [UIImage(named: "kalen-emsley-98265-unsplash"),UIImage(named: "kalen-emsley-98265-unsplash.jpg"),UIImage(named: "kalen-emsley-98265-unsplash.jpg")]
     
-    
+//    fileprivate func fetchFacebookUser() {
+//
+//        let graphRequestConnection = GraphRequestConnection()
+//        let graphRequest = GraphRequest(graphPath: "me", parameters: ["fields": "id, email, name, picture.type(large)"], accessToken: AccessToken.current, httpMethod: .GET, apiVersion: .defaultVersion)
+//        graphRequestConnection.add(graphRequest, completion: { (httpResponse, result) in
+//            switch result {
+//            case .success(response: let response):
+//
+//                guard let responseDict = response.dictionaryValue else { Service.dismissHud(self.hud, text: "Error", detailText: "Failed to fetch user.", delay: 3); return }
+//
+//                let json = JSON(responseDict)
+//                self.name = json["name"].string
+//                self.email = json["email"].string
+//                guard let profilePictureUrl = json["picture"]["data"]["url"].string else { Service.dismissHud(self.hud, text: "Error", detailText: "Failed to fetch user.", delay: 3); return }
+//                guard let url = URL(string: profilePictureUrl) else { Service.dismissHud(self.hud, text: "Error", detailText: "Failed to fetch user.", delay: 3); return }
+//
+//                URLSession.shared.dataTask(with: url) { (data, response, err) in
+//                    if err != nil {
+//                        guard let err = err else { Service.dismissHud(self.hud, text: "Error", detailText: "Failed to fetch user.", delay: 3); return }
+//                        Service.dismissHud(self.hud, text: "Fetch error", detailText: err.localizedDescription, delay: 3)
+//                        return
+//                    }
+//                    guard let data = data else { Service.dismissHud(self.hud, text: "Error", detailText: "Failed to fetch user.", delay: 3); return }
+//                    self.profileImage = UIImage(data: data)
+//                    self.saveUserIntoFirebaseDatabase()
+//
+//                    }.resume()
+//
+//                break
+//            case .failed(let err):
+//                Service.dismissHud(self.hud, text: "Error", detailText: "Failed to get Facebook user with error: \(err)", delay: 3)
+//                break
+//            }
+//        })
+//        graphRequestConnection.start()
+//
+//
+//    }
    
     @IBAction func loginWithFB(_ sender: UIButton){
         //Firstly login with FB SDK
